07ed7a0e2a729490bde106a1f4041a21bbc7d780
anandbaraik <anandbaraik2014@gmail.com>
2023-05-18 02:03:04 +0530

    footer & header completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,71 @@
 import React from 'react'
 import "./Footer.css";
+import { NavLink } from "react-router-dom";
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import AutoStoriesOutlinedIcon from '@mui/icons-material/AutoStoriesOutlined';
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="footer-company">
+        <h1 className='text-center'>
+        <AutoStoriesOutlinedIcon id='footer_bucher_icon'/>
+          Bücher
+        </h1>
+        <p className='text-center'>Because there is no friend as loyal as a book.!</p>
+        <p className='text-center'>&#169; 2023 Bücher</p>
+      </div>
+      <div className="footer-connect">
+        <h2>Connect with me</h2>
+        <p>Interested to collaborate? Feel free to connect over <span>👇🏼</span></p>
+        <div className="footer-connect-links">
+          <NavLink
+            className="navlink"
+            to="https://anandbaraik.github.io"
+            target="_blank"
+          >
+            <LanguageOutlinedIcon />
+          </NavLink>
+          <NavLink
+            className="navlink"
+            to="https://twitter.com/baraik_anand"
+            target="_blank"
+          >
+            <TwitterIcon />
+          </NavLink>
+          <NavLink
+            className="navlink"
+            to="https://github.com/anandbaraik"
+            target="_blank"
+          >
+            <GitHubIcon />
+          </NavLink>
+          <NavLink
+            className="navlink"
+            to="https://www.linkedin.com/in/anandbaraik/"
+            target="_blank"
+          >
+            <LinkedInIcon />
+          </NavLink>
+        </div>
+      </div>
+      <div className="footer-quick-links">
+        <h2 className='text-center'>Quick links</h2>
+        <div className="footer-quick-links-list">
+            <NavLink className="navlink" to="/signup">
+              Sign Up
+            </NavLink>
+            <NavLink className="navlink" to="/books">
+              Books
+            </NavLink>
+            <NavLink className="navlink" to="/wishlist">
+              Wishlist
+            </NavLink>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import "./Navbar.css";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import AutoStoriesOutlinedIcon from '@mui/icons-material/AutoStoriesOutlined';
 const Navbar = () => {
   return (
     <div className="navbar">
-      <Link to="/" className='navlink navbar_heading'>
-        <h1 className='navbar_heading'>
-          AnandKart
+      <NavLink className="navlink navbar_heading" to="/">
+        <h1 className="navbar_heading logo">
+          <AutoStoriesOutlinedIcon id='navbar_bucher_icon'/>
+          Bücher
         </h1>
-      </Link>
-      <div className='navbar_search'>
-        <input/>
+      </NavLink>
+      <div className="navbar_action">
+        <NavLink className="navlink wishlist" to="/wishlist">
+          <FavoriteBorderOutlinedIcon />
+          <span className='badge'>0</span>
+        </NavLink>
+        <NavLink className="navlink cart" to="/cart">
+          <LocalMallOutlinedIcon />
+          <span className='badge'>0</span>
+        </NavLink>
+        <NavLink className="navlink user" to="/profile">
+          <AccountCircleOutlinedIcon />
+        </NavLink>
       </div>
-      <div className='navbar_actions'>
-      <Link to="/" className='navlink wishlist'>
-        Wishlist
-      </Link>
-      <Link to="/" className='navlink wishlist'>
-        Cart
-      </Link>
-      <Link to="/" className='navlink wishlist'>
-        Profile
-      </Link>
+      <div className="navbar_search">
+        <input placeholder='Search books...' className="search"/>
+        <SearchOutlinedIcon className='secondary-color'/>
       </div>
     </div>
   )

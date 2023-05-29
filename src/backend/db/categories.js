80edaf2447e90e26b617c34d561b393dc6262b08
anandbaraik <anandbaraik2014@gmail.com>
2023-05-29 22:24:43 +0530

    categories updated

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -7,39 +7,45 @@ import { v4 as uuid } from "uuid";
 
 export const categories = [
   {
-    _id: uuid(),
-    categoryName: "Romance",
-    description:
-      "Remonatic books places its primary focus on the relationship and romantic love between two people, and usually has an emotionally satisfying and optimistic ending.",
+    "_id": "b9c0e5a7-8110-4743-b434-f38a1171556b",
+    "categoryName": "Romance",
+    "description": "Remonatic books places its primary focus on the relationship and romantic love between two people, and usually has an emotionally satisfying and optimistic ending.",
+    "coverImg": "https://github.com/anandbaraik/anandbaraik/assets/31516195/e381f513-147f-4266-af5f-1ac18fcd5436",
+    "id": "1"
   },
   {
-    _id: uuid(),
-    categoryName: "Adventure",
-    description:
-      "Adventure fiction is a type of fiction that usually presents danger, or gives the reader a sense of excitement.",
+    "_id": "920142ad-30b2-4e95-b47d-36ddc3253a1f",
+    "categoryName": "Adventure",
+    "description": "Adventure fiction is a type of fiction that usually presents danger, or gives the reader a sense of excitement.",
+    "coverImg": "https://github.com/anandbaraik/anandbaraik/assets/31516195/35481d9f-14d3-4b7d-b44f-a2146ddcf3b3",
+    "id": "2"
   },
   {
-    _id: uuid(),
-    categoryName: "Mystery",
-    description:
-      "Mystery is a fiction genre where the nature of an event, usually a murder or other crime, remains mysterious until the end of the story.",
+    "_id": "6520efaa-b538-4c5c-b71d-bd7488b71d5b",
+    "categoryName": "Mystery",
+    "description": "Mystery is a fiction genre where the nature of an event, usually a murder or other crime, remains mysterious until the end of the story.",
+    "coverImg": "https://github.com/anandbaraik/anandbaraik/assets/31516195/7aec76ed-909e-4c67-a0bc-df65c319617b",
+    "id": "3"
   },
   {
-    _id: uuid(),
-    categoryName: "Fantasy",
-    description:
-      "Fantasy books involve magical elements, typically set in a fictional universe and sometimes inspired by mythology and folklore.",
+    "_id": "1371797c-add7-43bc-b355-00097cf17e87",
+    "categoryName": "Fantasy",
+    "description": "Fantasy books involve magical elements, typically set in a fictional universe and sometimes inspired by mythology and folklore.",
+    "coverImg": "https://github.com/anandbaraik/anandbaraik/assets/31516195/3382976a-a7d3-4517-8b31-f23885f55a14",
+    "id": "4"
   },
   {
-    _id: uuid(),
-    categoryName: "Non-Fiction",
-    description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+    "_id": "cd3ccf15-4c38-449a-bfa7-942faf0b057f",
+    "categoryName": "Non-Fiction",
+    "description": "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+    "coverImg": "https://github.com/anandbaraik/anandbaraik/assets/31516195/a80d0f61-024d-4458-a8e1-156b6458edd2",
+    "id": "5"
   },
   {
-    _id: uuid(),
-    categoryName: "Science",
-    description:
-      "Science books are about the systematic and organizing of knowledge in the form of testable explanations and predictions about the universe.",
-  },
+    "_id": "4b7044ec-ce2a-4fe9-b789-2fee8a191d9e",
+    "categoryName": "Science",
+    "description": "Science books are about the systematic and organizing of knowledge in the form of testable explanations and predictions about the universe.",
+    "coverImg": "https://github.com/anandbaraik/anandbaraik/assets/31516195/d3c3fedf-5981-491a-a531-717f779c1d20",
+    "id": "6"
+  }
 ];
